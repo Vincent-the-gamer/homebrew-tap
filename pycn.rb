6bf12c84c5e544347bf8bd2243219d775b0cda1a
Mayu-chan <64945568+Vincent-the-gamer@users.noreply.github.com>
2025-07-26 11:54:22 +0800

    chore: update.

--- pycn.rb
+++ pycn.rb
@@ -3,7 +3,7 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Pycn < Formula
   desc "Write Python code in Chinese, just for fun ～(∠・ω< )⌒★"
-  homepage "https://mayu.vince-g.xyz/code-runner"
+  homepage "https://github.com/Vincent-the-gamer/pycn"
   url "https://github.com/Vincent-the-gamer/pycn/releases/download/v1.0.2/pycn_darwin_aarch64-1.0.2.tar.gz"
   sha256 "983b8d45cb375089b39422ccc667ee6fb768dd49187ea6d309d0fcbd85cd97d0"
   license ""
